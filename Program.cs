7bbe46b60857d39963e259953ff75317a8e4de10
bozturkk <89193311+bozturkk@users.noreply.github.com>
2022-03-11 18:00:48 +0300

    ders tamamlanmıştır.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IfElseIf // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if(time >= 6 && time < 11) // klasik if yapısı
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if(time <= 18)
+            {
+                Console.WriteLine("İyi günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi akşamlar");
+            }
+
+            string sonuc = time >= 6 && time < 11 ? "Günaydın" : time <= 18 ? "İyi günler" : "İyi akşamlar"; //single line if yapısı
+            
+
+            
+            Console.WriteLine(sonuc);
+        }
+    }
+}
